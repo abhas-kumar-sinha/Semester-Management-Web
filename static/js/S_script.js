4d11fed9626822ce1e7ba8536a542a8d4ce5ff79
Abhas Kumar Sinha <abhas.kumar22027@gmail.com>
2024-11-26 21:33:19 +0530

    fixed schedule - 1

--- static/js/S_script.js
+++ static/js/S_script.js
@@ -173,7 +173,7 @@ editBtn.addEventListener('click', () => {
 document.addEventListener('DOMContentLoaded', function() {
     // List of available time options
     const timeOptions = [
-        "8:00 AM", "9:00 AM", "10:00 AM", "11:00 AM", "1:00 PM", "2:00 PM", "3:00 PM", "4:00 PM", "5:00 PM", "6:00 PM"
+        "8:00 AM", "9:00 AM", "10:00 AM", "11:00 AM", "12:00 PM", "1:00 PM", "2:00 PM", "3:00 PM", "4:00 PM", "5:00 PM", "6:00 PM"
     ];
 
     // Function to convert time string (e.g., "1:00 PM") into an hour and minute object

93ac37656cca7cfb87d73410b95d8ad4ff08f98d
Abhas Kumar Sinha <abhas.kumar22027@gmail.com>
2024-12-01 03:00:41 +0530

    partially optimised for mobile  view

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ result = urlparse(database_url)
 connection = pg8000.connect(
     user=result.username,
     password=result.password,
-    host=result.hostname,
+    host="dpg-csrk7jl2ng1s738a9hag-a.oregon-postgres.render.com",
     port=5432,
     database=result.path[1:]
 )
